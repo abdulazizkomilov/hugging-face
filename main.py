424d59e594ef2bfdb6194e3f77f8f040915f9bad
Abdulaziz Komilov <80712573+abdulazizkomilov@users.noreply.github.com>
2024-10-12 08:12:57 +0500

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 
-model_id = "openai/whisper-base"
+model_id = "openai/whisper-large-v3"
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
 )
