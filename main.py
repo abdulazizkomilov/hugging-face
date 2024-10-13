ec0cf7ada584dc6325d59069403ddec55d097f69
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-10-13 08:03:12 +0500

    changed

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ app = FastAPI()
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-# model_id = "openai/whisper-large-v3"
-model_id = "openai/whisper-large-v3-turbo"
+model_id = "openai/whisper-large-v3"
+# model_id = "openai/whisper-tiny"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
