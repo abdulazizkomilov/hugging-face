35ea80c008b8dd5a5cfda876549b8ee8a93ed4ab
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-11-11 09:59:49 +0500

    added

--- unzipfile.py
+++ unzipfile.py
@@ -1,5 +1,5 @@
 import zipfile
 
-output = "stt_model.zip"
+output = "super-mixed-wav2vec2-1b-30-04-2024.zip"
 with zipfile.ZipFile(output, 'r') as zip_ref:
-    zip_ref.extractall("stt_model")
+    zip_ref.extractall("super-mixed-wav")
