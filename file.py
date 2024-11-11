6e8c2e38aeafa537851c01fe6ef2aef5913bc036
Abdulaziz Komilov <abdulazizkomilov2001@gmail.com>
2024-11-11 09:54:21 +0500

    added

--- file.py
+++ file.py
@@ -7,6 +7,7 @@ print("Downloading file...", config("FILE_ID"))
 file_id = os.getenv("FILE_ID")
 output = "super-mixed-wav.zip"
 
-gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False)
+gdown.download(f"https://drive.google.com/uc?id={file_id}", output, quiet=False, confirm=True)
+
 
 
